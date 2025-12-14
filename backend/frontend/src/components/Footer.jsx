41b702351309d6b24aa874ffb66d73748dd7e668
sadaf-saleem <sadafchanda0@gmail.com>
2025-12-15 03:57:08 +0500

    Remove accidentally added frontend folder from backend repo

--- backend/frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-4 text-center">
-      &copy; 2025 MyStore. All rights reserved.
-    </footer>
-  );
-}
